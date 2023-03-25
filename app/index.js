bb597d88ec087483ba6d01be4fda3d31eb3fd367
suryaant <gdswiyasa@gmail.com>
2023-03-25 11:28:54 +0800

    create detail job by id

--- app/index.js
+++ app/index.js
@@ -34,7 +34,7 @@ const Home = () => {
         <View style={{ flex: 1, padding: SIZES.medium }}>
           <Welcome />
           <Popularjobs />
-          <Nearbyjobs />
+          {/* <Nearbyjobs /> */}
         </View>
       </ScrollView>
     </SafeAreaView>
