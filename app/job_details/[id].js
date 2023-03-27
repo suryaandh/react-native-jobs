edf7d765b5e38cead81ae8874a7e3b8f26331686
suryaant <gdswiyasa@gmail.com>
2023-03-27 20:26:00 +0800

    add logic api to detail

--- app/job_details/[id].js
+++ app/job_details/[id].js
@@ -70,7 +70,16 @@ const JobDetails = () => {
           ) : data.length === 0 ? (
             <Text>No Data</Text>
           ) : (
-            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}></View>
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+
+              <JobTabs />
+            </View>
           )}
         </ScrollView>
       </>
