bacf35700ba5bc6b0f0811808f908c35680011ca
suryaant <gdswiyasa@gmail.com>
2023-05-05 12:09:50 +0800

    update component at job

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 
 import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
@@ -9,8 +11,8 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
       <View style={styles.logoBox}>
         <Image
           source={{
-            uri: checkImageURL(item.employer_logo)
-              ? item.employer_logo
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
           style={styles.logoImage}
@@ -22,13 +24,15 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
       </View>
 
       <View style={styles.companyInfoBox}>
-        <Text style={styles.companyName}>{companyName}</Text>
-        <Image
-          source={icons.location}
-          resizeMode="contain"
-          style={styles.locationImage}
-        />
-        <Text style={styles.locationName}>{location}</Text>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
       </View>
     </View>
   );
