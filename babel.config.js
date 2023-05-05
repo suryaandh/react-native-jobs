84cbd6e35d3056178d666c85ee0f3dcb3ef5be0f
suryaant <gdswiyasa@gmail.com>
2023-05-05 12:10:04 +0800

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
@@ -6,6 +6,17 @@ module.exports = function (api) {
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
     ],
   };
 };
